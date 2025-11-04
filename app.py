b54e1a4d3772b5350e13121ff4f35ac34c0c1810
leeda-ghassan <leeda.ghassan@yahoo.com>
2025-11-04 15:54:32 +0300

    add new route

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def register_process():
 @app.route("/todo_get") 
 def todo_get(user_id):
     user_id = UUID(user_id)
-    todos = todo_services.get_user_todos(user_id)
+    todos = todo_services.get_todo(user_id)
 
     return render_template("todo.html", todos=todos)
 
@@ -63,25 +63,40 @@ def todo_get(user_id):
 def todo(data):
     return render_template("todo.html", data=data)
 
-@app.route("/todo_update") #updating todo
+
+@app.route("/todo_update", methods=["POST"])
 def todo_update():
-    return render_template("todo.html")
+    todo_id = request.form["todo_id"]
+    title = request.form["title"]
+    description = request.form["description"]
+    # add update todo function from services
+    
+    return redirect(url_for("todo_get", user_id=request.form["user_id"]))
 
 @app.route("/sub_todo_get") #get sub_todo
 def sub_todo_get(todo_id):
     todo_id = UUID(todo_id)
-    subtodos = todo_services.get_sub_todos(todo_id)
+    subtodos = todo_services.get_sub_todo(todo_id)
     return render_template("sub_todo.html", subtodos=subtodos)
 
-@app.route("/sub_todo")  
+
+
+@app.route("/sub_todo", methods=["POST"])
 def sub_todo():
-    return render_template("sub_todo.html")
+    todo_id = request.form.get("todo_id")
+    title = request.form.get("title")
+    
+    return redirect(url_for("update_todo", todo_id=todo_id))
 
 
 @app.route("/sub_todo_update") #updating todo
 def sub_todo_update():
     return redirect(url_for("sub_todo"))
 
+@app.route("/sub_todo_update", methods=["POST"])
+def sub_todo_update():
+    todo_id = request.form.get("todo_id")
+    return redirect(url_for("update_sub_todo", todo_id=todo_id))
 
 
 
